7ce41177060e4dca16cc0137acfc5e6730965efa
ChihMin <s102062111@m102.nthu.edu.tw>
2016-10-18 17:04:31 +0800

    1. Refactor code to __main__

--- server/profiling-dump.py
+++ server/profiling-dump.py
@@ -4,15 +4,28 @@ import os, sys
 import redis
 import argparse
 
-parser = argparse.ArgumentParser(description='Dump profiling data from redis DB')
-parser.add_argument('-o', help="output filename")
-parser.add_argument('--port', help="redis db port number")
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description='Dump profiling data from redis DB')
+    parser.add_argument('--o', help="output filename")
+    parser.add_argument('--port', help="redis db port number")
+    args = parser.parse_args()
+    file = None
 
-portnumber = 6379
-if args.port is not None:
-    portnumber = int(args.port)
+    portnumber = 6379
+    if args.port is not None:
+        portnumber = int(args.port)
+    
+    if args.o is not None:
+        file = open(args.o, "w")    
 
-r = redis.Redis(port=portnumber)
-for key in r.keys():
-    print("{%s: %s}" % (key, r.hgetall(key)))
+    r = redis.Redis(port=portnumber)
+    for key in r.keys():
+        outputstr = "{%s: %s}" % (key, r.hgetall(key))
+        if file is None:
+            print(outputstr)
+        else:
+            file.write(outputstr)
+            file.write("\n")
+
+if __name__ == "__main__":
+    main()
--- server/profiling-parser.py
+++ server/profiling-parser.py
@@ -4,43 +4,47 @@ import redis
 import os, sys
 import argparse
 
-parser = argparse.ArgumentParser(description='Profiling database system')
-parser.add_argument('--file', help="profiling file path")
-parser.add_argument('--port', help="redis DB port number")
-args = parser.parse_args()
+def main():
+    parser = argparse.ArgumentParser(description='Profiling database system')
+    parser.add_argument('--file', help="profiling file path")
+    parser.add_argument('--port', help="redis DB port number")
+    args = parser.parse_args()
 
 
-if args.file is None:
-    print("Please input filename!")
-    exit(-1)
+    if args.file is None:
+        print("Please input filename!")
+        exit(-1)
 
-portnumber = 6379
-if args.port is not None:
-    portnumber = int(args.port)
+    portnumber = 6379
+    if args.port is not None:
+        portnumber = int(args.port)
 
-print("Current port number is %s, reading date from redis DB..." % portnumber)
+    print("Current port number is %s, reading date from redis DB..." % portnumber)
 
-r = redis.Redis(port=portnumber)
-filename = args.file
+    r = redis.Redis(port=portnumber)
+    filename = args.file
 
-file = open(filename, 'r')
-while True:
-    str = file.readline()
-    if len(str) == 0:
-        break
+    file = open(filename, 'r')
+    while True:
+        str = file.readline()
+        if len(str) == 0:
+            break
 
-    status = str.split()
-    nodename = status[6]
-    execname = status[7]
-    eventname = status[4]
-    
-    counts = int(status[3])
-    origincounts = r.hget(nodename, eventname)
-    if origincounts is not None:
-        counts = counts + int(origincounts)
+        status = str.split()
+        nodename = status[6]
+        execname = status[7]
+        eventname = status[4]
+        
+        counts = int(status[3])
+        origincounts = r.hget(nodename, eventname)
+        if origincounts is not None:
+            counts = counts + int(origincounts)
 
-    r.hset(nodename, 'execname', execname)
-    r.hset(nodename, eventname, counts)
+        r.hset(nodename, 'execname', execname)
+        r.hset(nodename, eventname, counts)
 
-r.save()
-file.close()
+    r.save()
+    file.close()
+
+if __name__ == "__main__":
+    main()
